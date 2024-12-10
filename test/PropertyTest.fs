61dcf2385562ea5eb539e945b114f661a0a42a9a
N0th1ngITMO <124551674+N0th1ngITMO@users.noreply.github.com>
2024-12-10 16:22:42 +0300

    Update PropertyTest.fs

--- test/PropertyTest.fs
+++ test/PropertyTest.fs
@@ -12,29 +12,37 @@ type ArbitraryHashMap<'k, 'v when 'k : comparison and 'v : comparison>() =
             gen {
                 let! size = Gen.choose (5, 20)
                 let! keyValuePairs = Gen.listOfLength size (Gen.zip (Arb.generate<'k>) (Arb.generate<'v>))
-                return List.fold (fun acc (k, v) -> acc.Add(k, v)) (SeparateChainingHashMap(10, hash)) keyValuePairs
+                return List.fold (fun (acc: SeparateChainingHashMap<'k, 'v>) (k, v) -> acc.Add k v ) (SeparateChainingHashMap(10, hash)) keyValuePairs
             }
 
 
 [<Property(Arbitrary = [| typeof<ArbitraryHashMap<int, string>> |])>]
 let ``Add does not change content for existing key-value pair`` (map: SeparateChainingHashMap<int, string>) key value =
-    let updatedMap = map.Add(key, value)
-    compare map updatedMap || (map.ToSet() = updatedMap.ToSet())
+    let updatedMap = map.Add key value
+    SeparateChainingHashMap.Compare map updatedMap || (map.ToSet() = updatedMap.ToSet())
 
 [<Property>]
 let ``Remove from empty map does not throw exception`` key =
     let emptyMap = SeparateChainingHashMap<int, string>(10, hash)
     let updatedMap = emptyMap.Remove(key)
-    Assert.Equal(emptyMap.ToSet(), updatedMap.ToSet())
+    Assert.True(SeparateChainingHashMap.Compare emptyMap updatedMap)
 
 [<Property(Arbitrary = [| typeof<ArbitraryHashMap<int, string>> |])>]
-let ``Merge is associative`` (map1: SeparateChainingHashMap<int, string>) (map2: SeparateChainingHashMap<int, string>) (map3: SeparateChainingHashMap<int, string>) =
-    let merged1 = SeparateChainingHashMap.Merge(map1, SeparateChainingHashMap.Merge(map2, map3))
-    let merged2 = SeparateChainingHashMap.Merge(SeparateChainingHashMap.Merge(map1, map2), map3)
-    compare merged1 merged2
+let ``Merge is associative`` (map1: SeparateChainingHashMap<int, string>) 
+                              (map2: SeparateChainingHashMap<int, string>) 
+                              (map3: SeparateChainingHashMap<int, string>) =
+
+    let merged1: SeparateChainingHashMap<int, string> = SeparateChainingHashMap.Merge map1 map2
+    let merged2: SeparateChainingHashMap<int, string> = SeparateChainingHashMap.Merge merged1 map3
+
+    let merged3: SeparateChainingHashMap<int, string> = SeparateChainingHashMap.Merge map2 map3
+    let merged4: SeparateChainingHashMap<int, string> = SeparateChainingHashMap.Merge map1 merged3
+
+    Assert.True(SeparateChainingHashMap.Compare merged2 merged4)
+
 
 [<Property>]
 let ``Compare maps with different data returns false`` (pairs1: (int * string) list) (pairs2: (int * string) list) =
-    let map1 = List.fold (fun acc (k, v) -> acc.Add(k, v)) (SeparateChainingHashMap(10, hash)) pairs1
-    let map2 = List.fold (fun acc (k, v) -> acc.Add(k, v)) (SeparateChainingHashMap(10, hash)) pairs2
-    pairs1 = pairs2 || not (compare map1 map2)
+    let map1 = List.fold (fun (acc: SeparateChainingHashMap<'k, 'v>) (k, v) -> acc.Add k v) (SeparateChainingHashMap(10, hash)) pairs1
+    let map2 = List.fold (fun (acc: SeparateChainingHashMap<'k, 'v>) (k, v) -> acc.Add k v) (SeparateChainingHashMap(10, hash)) pairs2
+    not (SeparateChainingHashMap.Compare map1 map2)
